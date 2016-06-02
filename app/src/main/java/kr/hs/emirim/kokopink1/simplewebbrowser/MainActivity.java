37a7c2e5f5015093ebde66f272997f66aef37b7b
Eunji Shim <eunji991222@gmail.com>
2016-06-02 20:57:17 +0900

    #2 finish program

--- app/src/main/java/kr/hs/emirim/kokopink1/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kokopink1/simplewebbrowser/MainActivity.java
@@ -18,13 +18,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         edit=(EditText)findViewById(R.id.edit);
         butMove=(Button)findViewById(R.id.but_move);
+        web.setWebViewClient(new WebViewClient());
+        WebSettings WebSet=web.getSettings();
+        WebSet.setBuiltInZoomControls(true);
+        WebSet.setJavaScriptEnabled(true);
         butPrev=(Button)findViewById(R.id.but_prev);
         web=(WebView)findViewById(R.id.web);
         butMove.setOnClickListener(this);
+        butPrev.setOnClickListener(this);
+        //web.loadUrl("http://www.naver.com");
     }
 
     @Override
     public void onClick(View v) {
-
+        String s="http://";
+        String s2=edit.getText().toString();
+        switch (v.getId())
+        {
+            case R.id.but_move:
+                if(s2.contains(s))
+                {
+                    web.loadUrl(s2);
+                }
+                else
+                {
+                    web.loadUrl(s+s2);
+                }
+                break;
+            case R.id.but_prev: web.goBack(); break;
+        }
     }
 }
